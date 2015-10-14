82481e298dbcd80fd784d0fd2ea94008b0d9d35b
janice dunn & abdul rahimi <student@epicodus.com>
2015-10-14 14:07:05 -0700

    add edit and delete links

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -19,6 +19,21 @@ class ContributionsController < ApplicationController
     @contribution = @story.contributions.find(params[:id])
   end
 
+  def edit
+    @story = Story.find(params[:story_id])
+    @contribution = @story.contributions.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:story_id])
+    @contribution = @story.contributions.find(params[:id])
+    if @contribution.update(contribution_params)
+      redirect_to story_path(@story)
+    else
+      render :edit
+    end
+  end
+
   private
     def contribution_params
       params.require(:contribution).permit(:username, :sentence)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :stories do
     resources :contributions
   end
+  
 end
