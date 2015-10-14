adb2a7c730402a663a5af0c8d44ad9dec4f0381c
janice dunn & abdul rahimi <student@epicodus.com>
2015-10-14 10:51:38 -0700

    Add delete route for story.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -18,7 +18,26 @@ class StoriesController < ApplicationController
     else
       render :new
     end
-end
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to stories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to stories_path
+  end
 
   private
   def story_params
